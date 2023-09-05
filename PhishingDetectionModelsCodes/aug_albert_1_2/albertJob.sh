ac2fa43af671d0dccd959a88a28b979ad20e4e08
parisa82gh <123210727+parisa82gh@users.noreply.github.com>
2023-09-05 14:32:44 -0500

    Add files via upload

---
+++ PhishingDetectionModelsCodes/aug_albert_1_2/albertJob.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J TrainALBERT
+#SBATCH -o TrainALBERT.o%j
+#SBATCH --mail-user=pmehdigholampour@uh.edu
+#SBATCH --mail-type=ALL
+#SBATCH --ntasks-per-node=1 -N 1
+#SBATCH -t 4:0:0
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-cpu=8GB
+
+python albert.py --train /project/verma/TextAttack_Parisa/augmented_data/train_1_2.csv  --test /project/verma/TextAttack_Parisa/data/test_emails.csv
+
---
+++ PhishingDetectionModelsCodes/aug_albert_1_4/textattckJob.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -J TextAttack
+#SBATCH -o TextAttack.o%j
+#SBATCH --mail-user=pmehdigholampour@uh.edu
+#SBATCH --mail-type=ALL
+#SBATCH --ntasks-per-node=1 -N 1
+#SBATCH -t 40:0:0
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-cpu=32GB
+
+textattack attack --model-from-huggingface /project/verma/TextAttack_Parisa/aug_albert_1_4/albert_model_1_4 --dataset-from-file /project/verma/TextAttack_Parisa/aug_albert/textAttack_dataset.py --recipe bae --num-examples 518
